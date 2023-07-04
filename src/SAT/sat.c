b095d9a3c5796f2189c96c4310ee0e31a44f81dc
Cicada <hubbleadd@bk.ru>
2023-07-04 16:42:56 +0700

    Replace MU again

--- src/SAT/sat.c
+++ src/SAT/sat.c
@@ -92,8 +92,8 @@ void sat_encode_command(const char* name, __int16_t* code, __int16_t* mem_cell, 
 }
 
 void sat_write_next_obj(
-        [[maybe_unused]] FILE* file,
-        [[maybe_unused]] __int16_t mem_cell,
+        __attribute__((unused)) FILE* file,
+        __attribute__((unused)) __int16_t mem_cell,
         __int16_t command_code,
         __int16_t operand,
         __int16_t* mem_ptr)
